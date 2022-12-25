865af6e18dbd704f251a4e9dcef17f37170e773e
kflmattukz <kflmattukz@gmail.com>
2022-12-25 16:33:19 +0800

    fix login, Auth using Jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,29 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  async signup(data: AuthDto): Promise<User | undefined> {
-    // generate a hash password
+  async signup(data: AuthDto): Promise<{ access_token: string }> {
     const hash = await argon.hash(data.password);
     try {
-      // create new user
       const user = await this.prisma.user.create({
         data: {
           email: data.email,
           hash,
         },
       });
-      // delete hash before return
-      delete user.hash;
-      // return new user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (
         error instanceof PrismaClientKnownRequestError &&
@@ -33,22 +33,38 @@ export class AuthService {
     }
   }
 
-  async signin(data: AuthDto): Promise<User | undefined> {
-    // find user by email
+  async signin(data: AuthDto): Promise<{ access_token: string }> {
     const user = await this.prisma.user.findUnique({
       where: {
         email: data.email,
       },
     });
-    // check if user exist, if not throw error
+
     if (!user) throw new ForbiddenException('Credetials Incorrect');
 
     const isMatch = await argon.verify(user.hash, data.password);
-    // chech if password is correct, if not throw error
+
     if (!isMatch) throw new ForbiddenException('Credetials Incorrect');
-    // delete hash before return
-    delete user.hash;
-    // return user;
-    return user;
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '1d',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
